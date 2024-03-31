b8fe0088afa9a10dbd20dccc75d7db29e21981d0
digitaladi <aladi.timera1605@gmail.com>
2024-03-31 12:22:29 +0200

    fichier documentation

--- docugithub.js
+++
@@ -1,7 +0,0 @@
-/* POUR COMMIT ET PUSH DANS GITHUB
-git add .
-git commit -m "ajout fichier "
-git branch -M main
-git remote add origin git@github.com:digitaladi/koinbio.git
-git push -u origin main
-*/
--- documentation/docugithub.js
+++ documentation/docugithub.js
@@ -4,13 +4,4 @@ git commit -m "ajout fichier "
 git branch -M main
 OPTIONEL : git remote add origin git@github.com:digitaladi/koinbio.git
 git push -u origin main
-
-
-
-
-
-
-
-
-
 */

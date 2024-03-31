ac99c3444219fdd91a4719ae30fab9c57fc839d0
digitaladi <aladi.timera1605@gmail.com>
2024-03-31 16:13:24 +0200

    TEST

--- documentation/docugithub.js
+++ documentation/docugithub.js
@@ -4,4 +4,10 @@ git commit -m "ajout fichier "
 git branch -M main
 OPTIONEL : git remote add origin git@github.com:digitaladi/koinbio.git
 git push -u origin main
+
+
+
+git remote set-url origin https://github.com/digitaladi/koinbio.git
+REDEFINIR L URL DE GITHUB 
+
 */
